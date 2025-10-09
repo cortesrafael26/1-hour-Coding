915c3b436212fb9d7659e02c2e41bbae1487babc
Rafael Louise Cortes <rafaellouise123@gmail.com>
2025-10-09 21:50:51 +0800

    Java OOP: Inheritance

--- day1/Cat.java
+++ day1/Cat.java
@@ -1,5 +1,3 @@
-package day1;
-
 public class Cat extends Animal {
     @Override
     public void makeSound() {
--- day1/Main.java
+++ day1/Main.java
@@ -1,5 +1,3 @@
-package day1;
-
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
