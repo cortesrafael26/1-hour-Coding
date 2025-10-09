c90293ea8e96ab39d84f0e4418e964d43c33f306
Rafael Louise Cortes <rafaellouise123@gmail.com>
2025-10-09 22:23:03 +0800

    Java OOP: Inheritance

--- day1/Main.java
+++ day1/Main.java
@@ -2,7 +2,17 @@ package day1;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.makeSound();
+        Person person = new Person("Rafael", "Cortes");
+        Employee employee = new Employee("Louise", "del Rosario", 2000354248, "Full Stack Developer");
+
+        System.out.println("PERSON");
+        System.out.println("First Name: " + person.getFirstName());
+        System.out.println("Last Name: " + person.getLastName() + "\n");
+
+        System.out.println("EMPLOYEE");
+        System.out.println("First Name: " + employee.getFirstName());
+        System.out.println("Last Name: " + employee.getLastName());
+        System.out.println("Employee ID: " + employee.getEmployeeId());
+        System.out.println("Job Title: " + employee.getJobTitle());
     }
 }
