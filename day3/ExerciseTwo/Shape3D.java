72ae2c8c53339169c5cbac125853331c95331f0f
Rafael Louise Cortes <rafaellouise123@gmail.com>
2025-10-11 22:15:34 +0800

    Java OOP: Abstraction

--- day3/ExerciseTwo/Shape3D.java
+++ day3/ExerciseTwo/Shape3D.java
@@ -1,5 +1,11 @@
 package day3.ExerciseTwo;
 
-public class Shape3D {
-    
+public abstract class Shape3D {
+    /* Write a Java program to create an abstract class Shape3D with abstract methods 
+    calculateVolume() and calculateSurfaceArea(). Create subclasses Sphere and Cube that 
+    extend the Shape3D class and implement the respective methods to calculate the volume 
+    and surface area of each shape. */
+
+    abstract double calculateVolume();
+    abstract double calculateSurfaceArea();
 }
