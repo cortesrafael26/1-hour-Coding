584aa2e8afafbbd8a5c33457098fd861291ba3b1
Rafael Louise Cortes <rafaellouise123@gmail.com>
2025-10-17 23:25:37 +0800

    Enums

---
+++ day9/ExerciseOne/Months.java
@@ -0,0 +1,16 @@
+package day9.ExerciseOne;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
