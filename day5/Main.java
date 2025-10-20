9fcc744e0c273003d214e9168ac4fcbc288cc826
Rafael Louise Cortes <145661413+cortesrafael26@users.noreply.github.com>
2025-10-20 19:10:27 +0800

    Java Enums

---
+++ day5/Main.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Main {
+    public static void main(String[] args) {
+        Months[] months = Months.values();
+
+        for (Months month : months) {
+            System.out.println(month);
+        }
+    }
+}
